f77e7639438928157a361fdf9180d463c163f3ac
Jurica Goricanec <jura.goricanec@gmail.com>
2022-11-03 11:29:13 +0100

    Add landing configuration changes

--- tmux_startup/start.sh
+++ tmux_startup/start.sh
@@ -11,8 +11,8 @@ rm .tmuxinator.yml
 
 # link the session file to .tmuxinator.yml
 # ln session_ar_tag_detection.yml .tmuxinator.yml
-ln session_sim_platform_landing.yml .tmuxinator.yml
-# ln session_rw_platform_landing.yml .tmuxinator.yml
+# ln session_sim_platform_landing.yml .tmuxinator.yml
+ln session_rw_platform_landing.yml .tmuxinator.yml
 
 
 # start tmuxinator
